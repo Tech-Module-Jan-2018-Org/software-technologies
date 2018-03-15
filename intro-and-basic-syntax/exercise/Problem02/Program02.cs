dfc9b1d7fd5ca492c654d4e4bb738e207a4d3bf1
denis1005 <denis_1005@abv.bg>
2018-03-15 22:15:49 +0200

    adding problem 02

---
+++ intro-and-basic-syntax/exercise/Problem02/Program02.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace P02_RectangleArea
+    {
+        public class P02_RectangleArea
+        {
+            public static void Main(string[] args)
+            {
+                var rectangleSideA = double.Parse(Console.ReadLine());
+                var rectangleSideB = double.Parse(Console.ReadLine());
+                var rectangleArea = rectangleSideA * rectangleSideB;
+
+                Console.WriteLine("{0:F2}", rectangleArea);
+            }
+        }
+    }
+
+   
